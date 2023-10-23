a0d1b6dd1ff772376488bfefbe612535c240122a
Eduardoespl <eduardoesp3317@gmail.com>
2023-10-23 15:09:43 -0600

    Added bassic operations

---
+++ index.py
@@ -0,0 +1,27 @@
+print("Estas es una calculadora simple")
+print("Ingresa el primer numero")
+num1 = int(input())
+print("Ingresa el segundo numero")
+num2 = int(input())
+print("Ingresa la operacion que deseas realizar")
+print("1. Suma")
+print("2. Resta")
+print("3. Multiplicacion")
+print("4. Division")
+operacion = int(input())
+
+if operacion == 1:
+    resultado = num1 + num2
+    print("El resultado es: " + str(resultado))
+elif operacion == 2:
+    resultado = num1 - num2
+    print("El resultado es: " + str(resultado))
+elif operacion == 3:
+    resultado = num1 * num2
+    print("El resultado es: " + str(resultado))
+elif operacion == 4:
+    resultado = num1 / num2
+    print("El resultado es: " + str(resultado))
+else:
+    print("Operacion no valida")
+    
